441e3ff9f2001873d30b0ac1babac297eed7a7da
Volodymyr <uuforreasonsunknownuu@gmail.com>
2024-12-24 13:40:05 +0200

    added str methods and property to models

--- train_station_system/models.py
+++ train_station_system/models.py
@@ -6,6 +6,9 @@ class Crew(models.Model):
     first_name = models.CharField(max_length=55)
     last_name = models.CharField(max_length=55)
 
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
 
 class Train(models.Model):
     name = models.CharField(max_length=55)
@@ -17,14 +20,27 @@ class Train(models.Model):
         related_name="trains"
     )
 
+    def __str__(self):
+        return f"{self.name} {self.cargo_num}"
+
+    @property
+    def number_of_seats(self):
+        return self.cargo_num * self.places_in_cargo
+
 
 class TrainType(models.Model):
     name = models.CharField(max_length=55)
+
+    def __str__(self):
+        return f"{self.name}"
 
 
 class Order(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.created_at}"
 
 
 class Ticket(models.Model):
@@ -41,11 +57,18 @@ class Ticket(models.Model):
         related_name="tickets"
     )
 
+    def __str__(self):
+        return (f"{self.cargo}, {self.seat}, "
+                f"{self.journey} {self.order}")
+
 
 class Station(models.Model):
     name = models.CharField(max_length=55)
     latitude = models.FloatField()
     longitude = models.FloatField()
+
+    def __str__(self):
+        return f"{self.name}"
 
 
 class Route(models.Model):
@@ -60,6 +83,9 @@ class Route(models.Model):
         related_name="routes"
     )
     distance = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.source} -> {self.destination}, {self.distance}"
 
 
 class Journey(models.Model):
@@ -76,3 +102,6 @@ class Journey(models.Model):
     departure_time = models.DateTimeField()
     arrival_time = models.DateTimeField()
     crew = models.ManyToManyField(Crew, related_name="journeys")
+
+    def __str__(self):
+        return f"{self.train}, {self.departure_time}, {self.arrival_time}"
